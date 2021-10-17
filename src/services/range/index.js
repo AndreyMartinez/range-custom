10c55d9cc1552394bbfda613b60e518394d42927
andreyMartinez <ram.v98@hotmail.com>
2021-10-17 12:43:48 -0500

    final first range

--- src/services/range/index.js
+++ src/services/range/index.js
@@ -1,10 +1,13 @@
+import axios from "axios";
+
+const URL = 'https://demo9961313.mockable.io'
 class RangeService {
     static findFirstExcercise() {
-
+     return axios.get(`${URL}/custom`);
     }
 
     static findSecondExcercise() {
-        
+    return axios.get(`${URL}/range`); 
     }
 }
 
